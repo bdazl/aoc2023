5b66c2c93af3e51de309461d0fec54bf9febb962
Jacob Peyron <jacob.peyron@gmail.com>
2023-12-02 15:32:02 +0100

    Day2 part 2

--- Day2.hs
+++ Day2.hs
@@ -56,13 +56,36 @@ isBagPossible = (all (==True)) . (map isElemPossible)
 isGamePossible :: Game -> Bool
 isGamePossible (id, bags) = all (==True) (map isBagPossible bags)
 
+getElemVal :: Bag -> String -> Int
+getElemVal [] _ = 0
+getElemVal ((n, id):bs) xs = if id == xs then n else getElemVal bs xs
+
+getRGB :: Bag -> (Int, Int, Int)
+getRGB bag = (r, g, b) where
+    r = getElemVal bag "red"
+    g = getElemVal bag "green"
+    b = getElemVal bag "blue"
+
+maxE :: (Int, Int, Int) -> (Int, Int, Int) -> (Int, Int, Int)
+maxE (r, g, b) (r', g', b') = (max r r', max g g', max b b')
+
+maxRGB :: [Bag] -> (Int, Int, Int)
+maxRGB bags = foldr maxE (0, 0, 0) rgbs where
+    rgbs = map getRGB bags
+
+power :: [Bag] -> Int
+power bags = r * g * b where
+    (r, g, b) = maxRGB bags
+
 solve :: [String] -> Int
 solve xs = sum ps where
     gs = parseAll xs
     ps = [id | (id, bags) <- gs, isGamePossible (id, bags)]
 
 solve' :: [String] -> Int
-solve' _ = -1
+solve' xs = sum pows where
+    gs = parseAll xs
+    pows = [power bags | (id, bags) <- gs]
 
 main :: IO ()
 main = do
@@ -71,14 +94,9 @@ main = do
     print $ solve l
     print $ solve' l
 
-
-ex1 :: [String]
-ex1 = ["Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green",
-       "Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue",
-       "Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red",
-       "Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red",
-       "Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green"]
-
-e1 = ex1 !! 0
-b1 = "3 blue, 4 red"
-bt1 = "3 blue, 4 red;"
+ex :: [String]
+ex = ["Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green",
+      "Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue",
+      "Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red",
+      "Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red",
+      "Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green"]
